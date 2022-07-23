5df57bb4c752235617927d6186554c4bc3fa0515
babaphillips <amandavphillips@icloud.com>
2022-07-22 20:55:02 -0400

    name input done

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
-const { generateMarkdown } = require('./utils/README.md');
+const { generateMarkdown } = require("./utils/README.md");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = () => {
-    return inquirer.prompt([
-        type: "input",
-    ])
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      },
+    },
+  ]);
 };
 
 // TODO: Create a function to write README file
